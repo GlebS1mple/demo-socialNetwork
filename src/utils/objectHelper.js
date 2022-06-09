2cbe733b4bc116439ad17c0bb327c8b782cf96f3
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-06-09 15:17:52 +0300

    Updates

--- src/utils/objectHelper.js
+++
@@ -1,8 +0,0 @@
-export const updateObjectInArray = (items, itemId, objPropName, newObjectProps) => {
-    return items.map((user) => {
-        if (user[objPropName] === itemId) {
-            return { ...user, ...newObjectProps }
-        }
-        return user;
-    })
-}
